e4651bfe7686579bc3de127b39d362fbf89dc1d1
CLAVIERES Antoine <antoine.clavieres@vignevin.com>
2026-07-27 10:31:39 +0200

    fix: envelopper App dans BrowserRouter dans main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,9 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   
